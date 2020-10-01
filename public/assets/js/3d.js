baa7af012ba8c44ee0a2deffa4e70c2d875bdffc
Brian Kovo <23661488+BrianKo14@users.noreply.github.com>
2020-10-01 19:24:17 -0300

    Further 3D

--- public/assets/js/3d.js
+++ public/assets/js/3d.js
@@ -2,55 +2,71 @@
 import { GLTFLoader } from 'https://unpkg.com/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.121.1/examples/jsm/controls/OrbitControls.js';
 
-const kitchenViewer = document.getElementById('3D-kitchen');
+function load3D(file, domElement, size, zPos, yPos) {
 
-// SCENE
-var scene = new THREE.Scene();
-scene.background = new THREE.Color('#ffffff');
+	// SCENE
+	var scene = new THREE.Scene();
+	scene.background = new THREE.Color('#ffffff');
 
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.rotation.y = 0;
-// camera.position.x = 800;
-camera.position.y = 0;
-camera.position.z = 6;
+	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+	// camera.rotation.x = 60;
+	// camera.position.x = 800;
+	camera.position.y = yPos;
+	camera.position.z = zPos;
 
-var renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+	var renderer = new THREE.WebGLRenderer({antialias:true});
+	renderer.setSize(size[0], size[1]);
 
-kitchenViewer.appendChild(renderer.domElement);
-
-
-// ORBIT
-let controls = new OrbitControls(camera, kitchenViewer.firstChild);
-controls.autoRotate = true;
-// controls.addEventListener('change', animate);
+	domElement.appendChild(renderer.domElement);
 
 
-// LOADER
-var loader = new GLTFLoader();
-loader.load( '3d_models/box.glb', function (gltf){
-	scene.add(gltf.scene);
-}, undefined, function (error){
-	console.error(error);
-});
+	// ORBIT
+	let controls = new OrbitControls(camera, domElement.firstChild);
+	controls.autoRotate = true;
+	controls.enablePan = false;
+	controls.enableZoom = false;
+	controls.minAzimuthAngle = 45;
+	controls.minAzimuthAngle = 45;
 
 
-// LIGHT
-var hlight = new THREE.AmbientLight(0x404040, 10);
-scene.add(hlight);
+	// LOADER
+	var loader = new GLTFLoader();
+	loader.load(file, function (gltf){
+		scene.add(gltf.scene);
+	}, undefined, function (error){
+		console.error(error);
+	});
 
-// var directionalLight = new THREE.DirectionalLight(0xffffff,100);
-// directionalLight.position.set(0,1,0);
-// directionalLight.castShadow = true;
-// scene.add(directionalLight);
+
+	// LIGHT
+	var hlight = new THREE.AmbientLight(0x404040, 10);
+	scene.add(hlight);
+
+	var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+	directionalLight.position.set(0,1,0);
+	directionalLight.castShadow = true;
+	scene.add(directionalLight);
 
 
-// LOOP
-function animate() {
-    requestAnimationFrame( animate );
+	// LOOP
+	function animate() {
+		requestAnimationFrame( animate );
 
-    controls.update();
+		controls.update();
 
-	renderer.render( scene, camera );
+		renderer.render( scene, camera );
+	}
+	animate();
 }
-animate();
+
+
+var url = window.location.pathname.split('/');
+url = url[url.length - 1];
+
+if (url == "cocinas.html") {
+	const pvc = document.getElementById('3D-pvc');
+	load3D('3d_models/box.glb', pvc, [0.6 * window.innerWidth/2, 0.6* window.innerHeight/2], 3, 0);
+}
+else if (url == "electrodomesticos.html") {
+
+}
